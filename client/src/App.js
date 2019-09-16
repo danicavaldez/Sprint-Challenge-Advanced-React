1ed6c739993380af0be6150325ca09671f709d8e
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 18:25:19 -0700

    Working on error from fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    } 
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(res => this.setState({ players: res }))
+  }
 
   render() {
     return(
