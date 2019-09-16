2c1dc23a1f35b632c729159ad14db542832a77fe
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-15 19:47:12 -0700

    Integration tests written for App and NavBar

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavBar from './components/NavBar'
+import { render } from "@testing-library/react"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
+
+test('renders without crashing', () => {
+  const div = document.createElement('div')
+  render(<NavBar />, div);
+});
+
+test('contains correct title in header', () => {
+  const {getByText} = render(<App />);
+  getByText("Women's World Cup Players")
+})
